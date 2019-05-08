5e758b9bdfae1bb543a24e41d33f97db3bb679dd
eboayue <keboayue@gmail.com>
2019-05-08 15:41:12 -0400

    Edit bash script to add Twitch logo to nginx default page

--- instance-setup.sh
+++ instance-setup.sh
@@ -5,3 +5,6 @@ sudo apt-get -y install wget
 sudo apt-get -y install nginx
 sudo service nginx start
 sudo apt -y install awscli
+cd /var/www/html/
+sudo aws s3 cp s3://twitchlogo/Twitch_logo.jpg .
+sudo sed -i '14 a <img src="TwitchLogo.png" alt="Twitch.tv Logo">' index.nginx-debian.html
